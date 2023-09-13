3a40ce37d06d7f4f15cc1a1f6866f39d36be718c
Vikelduk <108426561+Vikelduk@users.noreply.github.com>
2023-09-13 15:10:19 -0300

    Add files via upload

--- js/PlayerArcher.js
+++ js/PlayerArcher.js
@@ -1,7 +1,5 @@
-class PlayerArcher 
-{
-  constructor(x, y, width, height) 
-  {
+class PlayerArcher {
+  constructor(x, y, width, height) {
     const options = {
       isStatic: true
     };
@@ -14,24 +12,20 @@ class PlayerArcher
 
     World.add(world, this.body);
 
-    Matter.Body.setAngle(this.body, -90);
+    Matter.Body.setAngle(this.body, -PI / 2); // -90 degree
   }
 
-  display() 
-  {
+  display() {
     var pos = this.body.position;
     var angle = this.body.angle;
-    console.log(angle)
 
-    if (keyIsDown(DOWN_ARROW) && angle < -73 ) 
-    {
-      angle += 1;
+    if (keyIsDown(DOWN_ARROW) && angle < -1.2) {
+      angle += 0.01;
       Matter.Body.setAngle(this.body, angle);
     }
 
-    if (keyIsDown(UP_ARROW) && angle > -103) 
-    {
-      angle -=1;
+    if (keyIsDown(UP_ARROW) && angle > -1.6) {
+      angle -= 0.01;
       Matter.Body.setAngle(this.body, angle);
     }
 
@@ -43,6 +37,3 @@ class PlayerArcher
     pop();
   }
 }
-
-
-
--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -1,26 +1,40 @@
 class PlayerArrow {
-  constructor(x, y, width, height) {
+  constructor(x, y, width, height, archerAngle) {
     var options = {
       isStatic: true,
       density: 0.1
     };
     this.width = width;
     this.height = height;
-    
     this.body = Bodies.rectangle(x, y, this.width, this.height, options);
-    
     this.image = loadImage("./assets/arrow.png");
+    this.archerAngle = archerAngle;
+    this.velocity = p5.Vector.fromAngle(archerAngle);
     World.add(world, this.body);
   }
 
   shoot(archerAngle) {
-    var velocity = p5.Vector.fromAngle(archerAngle);
-    velocity.mult(20);
+    this.velocity = p5.Vector.fromAngle(archerAngle + PI / 2);
+    this.velocity.mult(55);
+
+    Matter.Body.setVelocity(this.body, {
+      x: this.velocity.x,
+      y: this.velocity.y
+    });
+
     Matter.Body.setStatic(this.body, false);
-    Matter.Body.setVelocity(this.body, { x: velocity.x, y: velocity.y });
   }
 
   display() {
+    var tmpAngle;
+    if (this.body.velocity.y === 0) {
+      tmpAngle = this.archerAngle + PI / 2;
+    } else {
+      tmpAngle = Math.atan(this.body.velocity.y / this.body.velocity.x);
+    }
+
+    Matter.Body.setAngle(this.body, tmpAngle);
+
     var pos = this.body.position;
     var angle = this.body.angle;
 
